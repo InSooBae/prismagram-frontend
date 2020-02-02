97844d947b84b298a7a8d9a407f4a8a3c16a4917
InSooBae <qospwmf@gmail.com>
2020-02-02 18:48:58 +0900

    add a Feed UI and Load component

---
+++ src/Components/Post/PostContainer.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import PostPresenter from './PostPresenter';
+import useInput from '../../Hooks/useInput';
+
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt,
+  caption,
+  location
+}) => {
+  //1개씩 늘어나고 없어지는거라 state에 임시적으로 담고 리프레쉬되면 실제서버로 바뀌고 아님 서버를 통해서가아닌 시각적인 효과로 바로 추가 및 삭제
+  const [isLikeds, setIsLiked] = useState(isLiked);
+  const [likeCounts, setLikeCount] = useState(likeCount);
+
+  const comment = useInput('');
+  return (
+    <PostPresenter
+      user={user}
+      files={files}
+      likeCount={likeCounts}
+      location={location}
+      caption={caption}
+      isLiked={isLikeds}
+      comments={comments}
+      createdAt={createdAt}
+      newComment={comment}
+      setIsLiked={setIsLiked}
+      setLikeCount={setLikeCount}
+    />
+  );
+};
+
+PostContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    userName: PropTypes.string.isRequired
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        userName: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
+};
+
+export default PostContainer;
--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,72 @@
-export default () => 'Feed';
+import React from 'react';
+import styled from 'styled-components';
+import { gql } from 'apollo-boost';
+import Helmet from 'react-helmet';
+import { useQuery } from '@apollo/react-hooks';
+import Loader from '../Components/Loader';
+import Post from '../Components/Post';
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        userName
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          userName
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 70vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QUERY);
+  return (
+    <Wrapper>
+      <Helmet>
+        <title>Feed | Prismagram</title>
+      </Helmet>
+      {loading && <Loader />}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => (
+          <Post
+            id={post.id}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            location={post.location}
+            caption={post.caption}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          />
+        ))}
+    </Wrapper>
+  );
+};
